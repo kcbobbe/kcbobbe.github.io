e340d4fc5c2d3bb8c8014f6cbd60eb42313438de
kcbobbe <kcbobbe@gmail.com>
2020-02-21 16:10:20 -0500

    about to try to switch to the SASS download of Materialize so that I can use variables to change style

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.parallax');
   var instances = M.Parallax.init(elems);
 
+  var tabElems = document.querySelectorAll('.tabs')
+  var instances = M.Tabs.init(tabElems);
 
-  var instance = M.Parallax.getInstance(elem);
+  // var instance = M.Parallax.getInstance(elem);
+  // var instance = M.Tabs.getInstance(tabElems);
+
 
 
 
