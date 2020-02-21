ac1b7f3623545b6d42eac2a5f68454df61063b3d
kcbobbe <kcbobbe@gmail.com>
2020-02-21 11:34:39 -0500

    initial setup of files, basic layout of nav and footer. Testing out components of Materialize, such as parallax

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+
+
+  var instance = M.Parallax.getInstance(elem);
+
+
+
+
+})
